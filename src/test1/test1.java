4d3d364bc242be475e60f6641ea58e3595f91b8e
yang <2016167323@qq.com>
2025-08-06 16:46:31 +0800

    hot-first commit1

--- src/test1/test1.java
+++ src/test1/test1.java
@@ -6,6 +6,7 @@ public class test1 {
         System.out.printf("Hello world");
         System.out.printf("Hello git");
         System.out.printf("Hello git2");
+        System.out.printf("Hello git3");
 
     }
 }
